efdacbf3eb5911f34074ec6a5b11461fa5969144
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-02-04 16:08:17 +0600

    Added Title Component

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,17 +1,85 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Title.css";
 
 class Title extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "This Is Title Of REACT DOM!",
+            isInput: false,
+        };
+    }
+
+    onClickHandeler = () => {
+        this.setState({
+            ...this.state,
+            isInput: true
+        })
+    };
+
+    onChangeHandler(event) {
+        this.setState({
+            ...this.state,
+            title: event.target.value,
+        })
+    }
+
+    enterHandler(event) {
+        if (event.type === 'blur') {
+            this.setState({
+                ...this.state,
+                isInput: false,
+            })
+        }
+    }
+
+    onKeyPressHandler(event) {
+        if (event.key === 'Enter') {
+            this.setState({
+                ...this.state,
+                isInput: false,
+            })
+        }
+    }
+
+
     render() {
+        let output = null;
+        if (this.state.isInput) {
+            output = (
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={(event) => this.onChangeHandler(event)}
+                    onKeyPress={(event) => this.onKeyPressHandler(event)}
+                    onBlur={(event) => this.enterHandler(event)}
+                    value={this.state.title}
+                />
+            )
+        } else {
+            output = (
+                <div className="row hover">
+                    <div className="col-8">
+                        <h1 className="display-5 title">{this.state.title}</h1>
+                    </div>
+                    <div className="col-4 icon">
+                    <span>
+                        <i className="fas fa-pencil-alt" onClick={() => this.onClickHandeler()}/>
+                    </span>
+                    </div>
+                </div>
+            );
+        }
         return (
             <div className="container mt-4">
                 <div className="row">
-                    <div className="col-8">
-                        <h1>This Is Title Of REACT DOM!</h1>
+                    <div className="col-12">
+                        {output}
                     </div>
                 </div>
             </div>
         );
     }
 }
+
 export default Title;
