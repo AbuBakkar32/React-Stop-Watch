ce34b7f45e217a3ce454f95b0e9db2f176cd2d12
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-02-05 14:32:11 +0600

    Added new component

---
+++ src/Components/CountDown/CountDown.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Digit from "./Digit/Digit";
+import "./CountDown.css";
+
+const CountDown = (props) => {
+    return (
+        <div className="d-flex my-4">
+            <Digit color="palegreen" value={ props.time.min }/>
+            <Digit color="skyblue" value={ props.time.sec }/>
+            <Digit color="salmon" value={ props.time.mili }/>
+        </div>
+    )
+}
+export default CountDown;
