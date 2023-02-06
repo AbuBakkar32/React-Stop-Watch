8befdf685934aaa74d99df0d231ce0f3a9844f5a
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-02-06 16:48:30 +0600

    Added new component

--- src/Components/CountDown/Digit/Digit.js
+++ src/Components/CountDown/Digit/Digit.js
@@ -4,7 +4,7 @@ import "./Digit.css";
 const Digit = (props) => {
     return (
         <div className="Digit mx-3 text-center" style={ { borderColor: props.color } }>
-            <h1 className="display-2 py-3" style={ { color: props.color } }>
+            <h1 className="display-2 py-4" style={ { color: props.color } }>
                 { props.value < 10 ? `0${props.value }` : props.value }
             </h1>
         </div>
