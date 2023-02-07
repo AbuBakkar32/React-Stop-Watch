fd628df10d1449ab43d589bcf2fdff88c3254b61
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-02-07 14:45:53 +0600

    Added new component

--- src/Components/CountDown/CountDown.js
+++ src/Components/CountDown/CountDown.js
@@ -5,9 +5,9 @@ import "./CountDown.css";
 const CountDown = (props) => {
     return (
         <div className="d-flex my-4 justify-content-center">
-            <Digit color="palegreen" value={ props.time.min }/>
-            <Digit color="skyblue" value={ props.time.sec }/>
-            <Digit color="salmon" value={ props.time.mili }/>
+            <Digit color="palegreen" value={ props.time.min } animate="animate__animated animate__pulse"/>
+            <Digit color="skyblue" value={ props.time.sec } animate="animate__animated animate__pulse"/>
+            <Digit color="salmon" value={ props.time.mili } animate="animate__animated animate__pulse"/>
         </div>
     )
 }
--- src/Components/CountDown/Digit/Digit.js
+++ src/Components/CountDown/Digit/Digit.js
@@ -3,7 +3,7 @@ import "./Digit.css";
 
 const Digit = (props) => {
     return (
-        <div className="Digit mx-3 text-center" style={ { borderColor: props.color } }>
+        <div className={"Digit mx-3 text-center " + props.animate} style={ { borderColor: props.color } }>
             <h1 className="display-2 py-4" style={ { color: props.color } }>
                 { props.value < 10 ? `0${props.value }` : props.value }
             </h1>
